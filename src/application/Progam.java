2f4de1b4e99f55df111e70985b49ca8d9d003d1a
Anderson <andersson_73@live.com>
2022-11-14 00:36:22 -0300

    CheckMate logic

--- src/application/Progam.java
+++ src/application/Progam.java
@@ -18,7 +18,7 @@ public class Progam {
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		
-		while(true) {
+		while(!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(chessMatch, captured);
@@ -49,5 +49,7 @@ public class Progam {
 				sc.nextLine();
 			}
 		}
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 }
